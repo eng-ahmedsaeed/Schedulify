a7b61c522fe8f89c3b101ab9b2da223fa9fba06f
eng-ahmedsaeed <ahmedsaeed.ah101@gmail.com>
2026-04-11 13:39:48 +0200

    Testing FCFS and Creating SJF

--- AlgoPkg/SJF.py
+++ AlgoPkg/SJF.py
@@ -1,4 +1,5 @@
 from ProcessPkg.Process import Process
+import copy
 class SJF:
     def __init__(self,ProcessList):
         self.ProcessList=ProcessList
@@ -7,24 +8,98 @@ class SJF:
  
 
     def SJFAlgoNonPreemptive (self):
+        Counter=0
+        SortedProcessList=[]
         #sorting proc3ess list according to arrival time and burst time
         self.ProcessList.sort(key=lambda p: (p.BurstTime,p.ArrivalTime))
-        #calculating start time and end time for each process
-        self.ProcessList[0].StartTime=0
-        self.ProcessList[0].EndTime=self.ProcessList[0].StartTime+self.ProcessList[0].BurstTime
-        self.ProcessList[0].TurnAround=self.ProcessList[0].EndTime-self.ProcessList[0].ArrivalTime
-        self.ProcessList[0].WaitingTime=self.ProcessList[0].TurnAround-self.ProcessList[0].BurstTime
-        self.AvgTurnAround+=self.ProcessList[0].TurnAround
-        self.AvgWaitingTime+=self.ProcessList[0].WaitingTime
-        for i in range(1,len(self.ProcessList)):
-            self.ProcessList[i].StartTime=self.ProcessList[i-1].EndTime
-            self.ProcessList[i].EndTime=self.ProcessList[i].StartTime+self.ProcessList[i].BurstTime
-            self.ProcessList[i].TurnAround=self.ProcessList[i].EndTime-self.ProcessList[i].ArrivalTime
-            self.ProcessList[i].WaitingTime=self.ProcessList[i].TurnAround-self.ProcessList[i].BurstTime
-            self.AvgTurnAround+=self.ProcessList[i].TurnAround
-            self.AvgWaitingTime+=self.ProcessList[i].WaitingTime
+        shortestProcess=min(self.ProcessList,key =lambda p:p.ArrivalTime)
+        SortedProcessList.append(shortestProcess)
+        self.ProcessList.remove(shortestProcess)
+        #calculating start time and end time for first process
+        SortedProcessList[0].StartTime=SortedProcessList[0].ArrivalTime
+        SortedProcessList[0].EndTime=SortedProcessList[0].StartTime+SortedProcessList[0].BurstTime
+        SortedProcessList[0].TurnAround=SortedProcessList[0].EndTime-SortedProcessList[0].ArrivalTime
+        SortedProcessList[0].WaitingTime=SortedProcessList[0].TurnAround-SortedProcessList[0].BurstTime
+        self.AvgTurnAround+=SortedProcessList[0].TurnAround
+        self.AvgWaitingTime+=SortedProcessList[0].WaitingTime
+        Counter+=SortedProcessList[0].EndTime
+        #searching for leastBurst avilable process
+        #calculating the ProcessProperities in the same iteration
+        while (len(self.ProcessList)):
+            shortestProcess=next((p for p in self.ProcessList if p.ArrivalTime<=Counter),None)
+            if(shortestProcess==None):
+                #finding the next processupon arrival
+                shortestProcess=min(self.ProcessList,key=lambda p:p.ArrivalTime)
+                #calculate the Prop
+                shortestProcess.StartTime=shortestProcess.ArrivalTime
+                shortestProcess.EndTime=shortestProcess.StartTime+shortestProcess.BurstTime
+                shortestProcess.TurnAround=shortestProcess.EndTime-shortestProcess.ArrivalTime
+                shortestProcess.WaitingTime=shortestProcess.TurnAround-shortestProcess.BurstTime
+                #Append in the sorted List and update the counter
+                SortedProcessList.append(shortestProcess)
+                self.ProcessList.remove(shortestProcess)
+                Counter=shortestProcess.EndTime
+            else:
+                shortestProcess.StartTime=Counter
+                shortestProcess.EndTime=shortestProcess.StartTime+shortestProcess.BurstTime
+                shortestProcess.TurnAround=shortestProcess.EndTime-shortestProcess.ArrivalTime
+                shortestProcess.WaitingTime=shortestProcess.TurnAround-shortestProcess.BurstTime
+                SortedProcessList.append(shortestProcess)
+                self.ProcessList.remove(shortestProcess)
+                Counter=shortestProcess.EndTime
+            self.AvgTurnAround+=shortestProcess.TurnAround
+            self.AvgWaitingTime+=shortestProcess.WaitingTime
 
+            
+        self.ProcessList=SortedProcessList
         self.AvgTurnAround=self.AvgTurnAround/len(self.ProcessList)
         self.AvgWaitingTime=self.AvgWaitingTime/len(self.ProcessList)
         return self.ProcessList,self.AvgWaitingTime,self.AvgTurnAround
-            
+
+    def SJFAlgoPreemptive (self): 
+        SortedProcessList=[] 
+        self.ProcessList.sort(key=lambda p: (p.RemainingTime,p.ArrivalTime))
+        Counter=0
+        FinishedProcess=0
+        while(len(self.ProcessList)!=FinishedProcess):
+            #intialize the minimum remaining time to infinity and selected process to null
+            minRemain= int('inf')
+            selectProcess=None
+            #searching for leastBurst avilable process
+            self.ProcessList.sort(key=lambda p: (p.ArrivalTime,p.RemainingTime))
+            for p in self.ProcessList:
+                if(p.ArrivalTime<=Counter and (p.RemainingTime<minRemain and p.RemainingTime>0)):
+                    minRemain=p.RemainingTime
+                    selectProcess=p
+            #if there is no process available increase the counter and continue searching
+            if(selectProcess==None):
+                Counter+=1
+                continue
+            #if there is a process available execute it for 1 unit of time
+            else:
+                # if The pocess is starting for the first time set the start time
+                if(selectProcess.RemainingTime==selectProcess.BurstTime):
+                    selectProcess.StartTime=Counter
+                selectProcess.RemainingTime-=1
+                #if the process is finished calculate the properties and update the counter
+                if(selectProcess.RemainingTime==0):
+                    self.ProcessList.EndTime=Counter+1
+                    FinishedProcess+=1
+                    ProcessTurnAround=selectProcess.EndTime-selectProcess.ArrivalTime
+                    selectProcess.TurnAroundTime=ProcessTurnAround
+                    self.AvgTurnAround+=ProcessTurnAround
+                    ProcessWaitingTime=ProcessTurnAround-selectProcess.BurstTime
+                    selectProcess.WaitingTime=ProcessWaitingTime
+                    self.AvgWaitingTimeAround+=ProcessWaitingTime
+                #creating a new process with burst time 1 to be added in the sorted list
+
+                CreatedProcess= copy.deepcopy(selectProcess)
+                CreatedProcess.StartTime=Counter
+                CreatedProcess.EndTime=Counter+1
+                CreatedProcess.BurstTime=1
+                SortedProcessList.append(CreatedProcess)
+                Counter +=1
+        return SortedProcessList,self.AvgWaitingTime,self.AvgTurnAround,self.ProcessList
+
+        
+        
--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from AlgoPkg.FCFS import FCFS
+from AlgoPkg.SJF import SJF
 from ProcessPkg.Process import Process
 
 def main():
     processes = [
-        Process(1, ArrivalTime=0, BurstTime=5),
-        Process(2, ArrivalTime=6, BurstTime=3),
-        Process(3, ArrivalTime=8, BurstTime=1),
-        Process(4, ArrivalTime=9, BurstTime=2)
-    ]
+            Process(1, ArrivalTime=1, BurstTime=7),
+            Process(2, ArrivalTime=3, BurstTime=4),
+            Process(3, ArrivalTime=5, BurstTime=1),
+            Process(4, ArrivalTime=7, BurstTime=2),
+            Process(5, ArrivalTime=13, BurstTime=5),
+            Process(6, ArrivalTime=14, BurstTime=2)
+        ]
  
-    Algo = FCFS(processes)
-    processes,AvgW,AvgT=Algo.FCFSAlgo()
+    Algo = SJF(processes)
+    processes,AvgW,AvgT=Algo.SJFAlgoNonPreemptive()
 
     for p in processes:
         print(p,"-")
